3a301972e5dd153b659896ed9f4710338820d192
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-02-17 19:38:56 +0100

    print_eval false

--- src/config.h
+++ src/config.h
@@ -23,7 +23,7 @@ constexpr tune_t initial_learning_rate = 1;
 constexpr int32_t learning_rate_drop_interval = 10000;
 constexpr tune_t learning_rate_drop_ratio = 1;
 constexpr bool print_data_entries = false;
-constexpr bool print_eval = true;
+constexpr bool print_eval = false;
 
 constexpr bool use_eval = true;
 constexpr int32_t wdl_percentage = 100;
