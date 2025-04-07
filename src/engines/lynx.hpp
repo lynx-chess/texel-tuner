8875d4dd0ee942ffdf1fd3ef8ea63fcb850fc92a
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-04-07 11:51:36 +0200

    Revert "Add enemy king to the equation"
    This reverts commit 5a0ab74889e41e7c5acc7dfe4138b772cb14e58a.

--- src/engines/lynx.hpp
+++ src/engines/lynx.hpp
@@ -31,7 +31,6 @@ const static int numParameters = psqtIndexCount +
 
                                  // Arrays
                                  FriendlyKingInFrontOfPassedPawnBonus.tunableSize +
-                                 EnemyKingInFrontOfPassedPawnPenalty.tunableSize +
                                  PawnPhalanxBonus.tunableSize +
                                  ConnectedRooksBonus.tunableSize +
                                  PawnIslandsBonus.tunableSize +
@@ -126,7 +125,6 @@ public:
 
         // Arrays
         FriendlyKingInFrontOfPassedPawnBonus.add(result);
-        EnemyKingInFrontOfPassedPawnPenalty.add(result);
         PawnPhalanxBonus.add(result);
         ConnectedRooksBonus.add(result);
         PawnIslandsBonus.add(result);
@@ -163,7 +161,6 @@ public:
         assert(RookMobilityBonus.tunableSize == 15);
         assert(QueenMobilityBonus.tunableSize == 28);
         assert(FriendlyKingInFrontOfPassedPawnBonus.tunableSize == 6);
-        assert(EnemyKingInFrontOfPassedPawnPenalty.tunableSize == 6);
 
         std::cout << result.size() << " == " << numParameters << std::endl;
         assert(result.size() == numParameters);
@@ -298,8 +295,6 @@ public:
         // Arrays
         name = NAME(FriendlyKingInFrontOfPassedPawnBonus);
         FriendlyKingInFrontOfPassedPawnBonus.to_csharp(parameters, ss, name);
-        name = NAME(EnemyKingInFrontOfPassedPawnPenalty);
-        EnemyKingInFrontOfPassedPawnPenalty.to_csharp(parameters, ss, name);
 
         name = NAME(PawnPhalanxBonus);
         PawnPhalanxBonus.to_csharp(parameters, ss, name);
@@ -419,9 +414,6 @@ public:
         // Arrays
         name = NAME(FriendlyKingInFrontOfPassedPawnBonus);
         FriendlyKingInFrontOfPassedPawnBonus.to_cpp(parameters, ss, name);
-        ss << "\n";
-        name = NAME(EnemyKingInFrontOfPassedPawnPenalty);
-        EnemyKingInFrontOfPassedPawnPenalty.to_cpp(parameters, ss, name);
         ss << "\n";
 
         name = NAME(PawnPhalanxBonus);
@@ -597,12 +589,12 @@ int PawnAdditionalEvaluation(int squareIndex, int pieceIndex, int bucket, int op
             IncrementCoefficients(coefficients, FriendlyKingInFrontOfPassedPawnBonus.index + rank - FriendlyKingInFrontOfPassedPawnBonus.start, color);
         }
 
-        // Enemy King in front of passed pawn
-        if (oppositeSideKingRank > rank)
-        {
-            packedBonus += EnemyKingInFrontOfPassedPawnPenalty.packed[rank];
-            IncrementCoefficients(coefficients, EnemyKingInFrontOfPassedPawnPenalty.index + rank - EnemyKingInFrontOfPassedPawnPenalty.start, color);
-        }
+        // // Enemy king in front of passed pawn
+        // if(oppositeSideKingRank < rank)
+        // {
+        //     packedBonus += EnemyKingInFrontOfPassedPawnPenalty.packed;
+        //     IncrementCoefficients(coefficients, EnemyKingInFrontOfPassedPawnPenalty.index, color);
+        // }
     }
 
     if (File[squareIndex] != 7 && GetBit(sameSidePawns, squareIndex + 1))
