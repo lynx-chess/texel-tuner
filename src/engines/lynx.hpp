07235788e2f03fcacfdea328c522bf85adfe95ef
Eduardo Càceres <eduardo.caceresdelacalle@gmail.com>
2025-12-26 10:53:39 +0100

    🧹 Cleanup

--- src/engines/lynx.hpp
+++ src/engines/lynx.hpp
@@ -1371,10 +1371,8 @@ int Threats(const chess::Board &board, const chess::Color &color, coefficients_t
 
     const auto nonPawnEnemies = __builtin_bswap64(board.them(color).getBits()) & ~theirPawns;
 
-    const auto safe = ~attacksBySide[static_cast<int>(~color)] |
+    const auto safeSquares = ~attacksBySide[static_cast<int>(~color)] |
                       (~attacks[static_cast<int>(chess::PieceType::PAWN) + oppositeSideoffset] & attacksBySide[color]);
-    // TODO: if we take into account all the piece attacks for defendedSquares
-    //| (evaluationContext.AttacksBySide[(int)Side] & ~evaluationContext.Attacks[oppositeSidePawnIndex]);
 
     auto pushes = ~__builtin_bswap64(board.occ().getBits()) & PawnPush(ourPawns, color);
 
@@ -1383,7 +1381,7 @@ int Threats(const chess::Board &board, const chess::Color &color, coefficients_t
     const auto doublePushes = ~__builtin_bswap64(board.occ().getBits()) & PawnPush(pushes & thirdRank, color);
     pushes |= doublePushes;
 
-    auto safePushes = pushes & safe;
+    auto safePushes = pushes & safeSquares;
     const auto pushThreats = PawnAttacks(safePushes, color) & nonPawnEnemies;
     const auto pushThreatsCount = chess::builtin::popcount(pushThreats);
 
